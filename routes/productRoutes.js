70011eff0b7489f78e688e6175da3a387a076150
Mohammad Rasool Rustam <95051802+mohammadrasool33@users.noreply.github.com>
2025-02-19 00:20:27 +0300

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,19 +4,11 @@ const router=express.Router();
 const Product=require('./../models/Product');
 const productController= require('../controllers/ProductController');
 
-// ✅ 1️⃣ Create a Product
 router.post('/api/products',productController.storeProduct);
-
-// ✅ 2️⃣ Read All Products
 router.get('/api/products',productController.getProducts);
-
-// ✅ 3️⃣ Read a Single Product by ID
 router.get('/api/products/:id', productController.getProduct);
-
 router.put('/api/products/:id',productController.updateProduct);
-
 router.delete('/api/products/:id',productController.deleteProduct);
-
 
 module.exports = router;
 
